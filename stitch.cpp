0a038986cdbfffa37d5c62898bc0591fa074a893
Zack Grannan <zgrannan@gmail.com>
2012-10-15 23:32:50 -0700

    Started work on image split test

--- stitch.cpp
+++ stitch.cpp
@@ -7,6 +7,33 @@
 using namespace std;
 using namespace cv;
 
+vector<Mat> getTestDataForImage(Mat image,
+								int rows,
+								int columns,
+								int horizontalOverlap,
+								int verticalOverlap){
+	vector<Mat> resultImages = vector<Mat>(rows * columns);
+	for (int j = 0; j < rows; j++){
+		for (int i = 0; i < cols; i++){
+			int imageX, imageY, imageWidth, imageHeight;
+			if (i == 0){
+				imageX = 0;
+				imageWidth = image->cols / image * ((horizontalOverlap-1)/2+1);
+			} else if (i == cols- 1){
+				imageWidth = image->cols / image * ((horizontalOverlap-1)/2+1);
+				imageX = image->cols - imageWidth;
+			}
+			
+			if (i == 0){
+				ImageY = 0;
+				imageHeight = image->cols / image * ((horizontalOverlap-1)/2+1);
+			} else if (i == cols- 1){
+				imageHeight = image->cols / image * ((horizontalOverlap-1)/2+1);
+				ImageY = image->cols - imageHeight;
+			}
+		}
+	}
+								}
 int main(){
 	
 	Mat image1 = imread("image1.jpg");
