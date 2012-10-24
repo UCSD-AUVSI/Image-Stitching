ae742b3984f7f4728c67c4c9a17c9402014d04e3
Zack Grannan <zgrannan@gmail.com>
2012-10-24 15:08:09 -0700

    Modifications to stitching

--- stitch.cpp
+++ stitch.cpp
@@ -10,50 +10,69 @@ using namespace cv;
 vector<Mat> getTestDataForImage(Mat image,
     int rows,
     int columns,
-    int horizontalOverlap,
-    int verticalOverlap){
+    double horizontalOverlap,
+    double verticalOverlap){
   vector<Mat> resultImages = vector<Mat>(rows * columns);
   int normalWidth = image.cols / columns;
   int normalHeight = image.rows / rows;
-  int overlapWidth = normalWidth * horizontalOverlap;
-  int overlapHeight = normalHeight * verticalOverlap;
+  int overlapWidth = (int)((double)(normalWidth) * horizontalOverlap);
+  int overlapHeight = (int)((double)(normalHeight) * verticalOverlap); 
+  cout << "Original Width: " << image.cols << "\n";
+  cout << "Original Height: " << image.rows << "\n";
+  cout << "Normal Width: " << normalWidth <<"\n";
+  cout << "Normal Height: " << normalHeight <<"\n";
+  cout << "Overlap Width: " << overlapWidth <<"\n";
+  cout << "Overlap Height: " << overlapHeight <<"\n";
+  cout << endl;
   int imageX, imageY, imageWidth, imageHeight;
   for (int j = 0; j < rows; j++){
     for (int i = 0; i < columns; i++){
-      imageX = min(i * normalWidth - overlapWidth,0);
+      imageX = max(i * normalWidth - overlapWidth,0);
       if (i == 0){
         imageWidth = min(normalWidth + overlapWidth, image.cols);
       } else {
         imageWidth = min(normalWidth + 2 * overlapWidth,image.cols - imageX);
       }
-      imageY = min(i * normalHeight - overlapHeight,0);
+      imageY = max(j * normalHeight - overlapHeight,0);
       if (i == 0){
         imageHeight = min(normalHeight + overlapHeight, image.rows);
       } else {
         imageHeight = min(normalHeight + 2 * overlapHeight ,image.rows - imageY);
       }
+      cout <<"Image "<<j * rows + i<<"\n";
+      cout <<"X: "<<imageX<<"\n";
+      cout <<"Y: "<<imageY<<"\n";
+      cout <<"Width: "<<imageWidth<<"\n";
+      cout <<"Height: "<<imageHeight<<"\n";
+      cout <<endl;
       resultImages[j * rows + i] = Mat(image,Range(imageY, imageY+imageHeight),Range(imageX,imageX +imageWidth));
     }
   }
   return resultImages;
 }
+
+Mat iterativeStitch(Mat accumulatedImage, vector<Mat> newImages) {
+  Mat result;
+  Stitcher stitcher = Stitcher::createDefault(true);
+  newImages.push_back(accumulatedImage);
+  if ( stitcher.stitch(newImages,result) ){
+    cout <<"Stitch successful";
+    return result;
+    }
+  else
+    return accumulatedImage;
+}
+
 int main(){
 
-  Mat image1 = imread("image1.jpg");
-  Mat image2 = imread("image.jpg");
-  Mat image3 = imread("image3.jpg");
-  Mat image4 = imread("image4.jpg");
-
-  //Stitcher* stitcher = new Stitcher();
-  Mat pano;
-  vector<Mat> images = vector<Mat>(4);
-  images[0] = image2;
-  images[1] = image1;
-  images[2] = image3;
-  images[3] = image4;
-
-  Stitcher stitcher = Stitcher::createDefault(true);
-  stitcher.stitch(images,pano);
+  Mat accumulator, pano;
+  vector<Mat> images = getTestDataForImage(imread("image.jpg"),2,2,0.2,0.2);
+  imwrite("a.jpg",images[0]);
+  imwrite("b.jpg",images[1]);
+  imwrite("c.jpg",images[2]);
+  imwrite("d.jpg",images[3]);
+  accumulator = images[3];
+  images.pop_back();
+  pano = iterativeStitch(accumulator,images);
   imwrite("result.jpg",pano);
-  system("open result.jpg");
 }
